380a5cb30c74acc67a4ca2e030a818a5565b85cd
Gwendoline Stéphan <stephag@trexvisu06.sis.cnes.fr>
2026-07-09 13:37:46 +0000

    Removing an unused variable from subroutine extract_params_from_pixc

--- scripts/process_mrf.py
+++ scripts/process_mrf.py
@@ -141,8 +141,7 @@ class MRF_run(object):
              mask,
              date,
              grid_coh_th_interp) = extract_params_from_pixc(file, root, ref_dem,
-                                                            min_crosstrack=self.cross_track_min,
-                                                            min_size=self.threshold)
+                                                            min_crosstrack=self.cross_track_min)
 
             fpdem.append([grid_h, grid_h_interp, grid_inc, grid_sig0, grid_sig0_interp,
                           grid_coh_interp, mask, date, grid_coh_th_interp])
